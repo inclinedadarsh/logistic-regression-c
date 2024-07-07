ff2f5129c8d3bec1a730540fb216bb4fb54fc8bb
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-07-07 22:21:44 +0530

    feat: add main.c file that trains a neuron and checks on the results

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -std=c99
-OBJS = matrix.o rand_float.o params.o activation.o forward.o cost.o backward.o optimize.o neuron.o
+OBJS = matrix.o rand_float.o params.o activation.o forward.o cost.o backward.o optimize.o neuron.o main.o
 
 output: $(OBJS)
 	$(CC) $(CFLAGS) $(OBJS) -o $@ -lm
 
 %.o: %.c %.h
 	$(CC) $(CFLAGS) -c $< -o $@ -lm
+
+main.o: main.c
+	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
 	rm -f (OBJS) output
---
+++ main.c
@@ -0,0 +1,40 @@
+#include "forward.h"
+#include "matrix.h"
+#include "neuron.h"
+#include "params.h"
+#include <stdbool.h>
+
+int main(void) {
+  // Defining the data
+  Matrix X = create_matrix(2, 4);
+  Matrix y = create_matrix(1, 4);
+
+  X.data[0][0] = 0;
+  X.data[1][0] = 0;
+  X.data[0][1] = 0;
+  X.data[1][1] = 1;
+  X.data[0][2] = 1;
+  X.data[1][2] = 0;
+  X.data[0][3] = 1;
+  X.data[1][3] = 1;
+
+  y.data[0][0] = 0;
+  y.data[0][1] = 0;
+  y.data[0][2] = 0;
+  y.data[0][3] = 1;
+
+  // Training the neuron
+  Params params = neuron(&X, &y, 1e-3, 40000, true, 42);
+
+  // Checking the results
+  Matrix a = forward_propagate(&params, &X);
+  print_matrix(&a);
+
+  // Freeing the memory
+  free_matrix(&X);
+  free_matrix(&y);
+  free_matrix(&a);
+  free_params(&params);
+
+  return 0;
+}
